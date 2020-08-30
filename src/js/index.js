d4c155ab9f0c67f726cb6ac67801722bab8c4429
Elena Kuznetsova <elenakuznetsova177@yandex.ru>
2020-08-30 17:25:23 +0300

    сделана версия верстки для планшета и мобилки, альтернативные шрифты, зафиксирваны версии плагинов, оформлен ридми

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,9 @@
 import "../css/style.css";
+
 // клик по "Показать еще" = добавится одна карточка (мало, да, но потом сделаю норм)
 // все кнопки в хедере для образца, в мобильной надо закомментировать лишние, иначе не влезают
 // клик по "Авторизоваться" = открыть попап авторизиции
 // клик по "Зарегистрироваься" в попапе Авторизации = открыть попап Регистрации
-
-// не смогла совладать с гридами в карточках, пока так, буду разбираться
 
 // иконки "Сохранить себе и Мусорка" только в одном состоянии,
 // потом сделаю шаблон с бэкграудом где они будут меняться
@@ -23,13 +22,48 @@ const preloaderSearching = document.getElementById("preloader-searching");
 const preloaderNotFound = document.getElementById("preloader-not-found");
 const showMoreResult = document.getElementById("search-result-show-more");
 const moreCards = document.querySelector(".search-result__card_off");
+const buttonMobileMenuMain = document.getElementById(
+  "header__menu-mobile-main-page",
+);
+const buttonCloseMobileMenu = document.querySelector(
+  ".header__menu-mobile_close",
+);
+const headerLogo = document.querySelector(".header__menu");
+const addCloseButtonHeader = document.querySelector(
+  ".header__menu-mobile_close",
+);
+const mobileMenuMain = document.querySelector(".header__buttons");
+const headerInfo = document.querySelector(".header__info");
+const searchResult = document.querySelector(".search-result");
+
+buttonMobileMenuMain.addEventListener("click", () => {
+  mobileMenuMain.classList.add("header__buttons_open");
+  headerLogo.classList.add("header_dark");
+  buttonMobileMenuMain.classList.add("header__menu-mobile_off");
+  addCloseButtonHeader.classList.add("header__menu-mobile_on");
+  headerInfo.classList.add("header__info_filter");
+  searchResult.classList.add("search-result_filter");
+});
+
+buttonCloseMobileMenu.addEventListener("click", () => {
+  mobileMenuMain.classList.toggle("header__buttons_open");
+  headerLogo.classList.toggle("header_dark");
+  buttonMobileMenuMain.classList.toggle("header__menu-mobile_off");
+  buttonCloseMobileMenu.classList.remove("header__menu-mobile_on");
+  headerInfo.classList.remove("header__info_filter");
+  searchResult.classList.remove("search-result_filter");
+});
 
 buttonClose.addEventListener("click", () => {
   popup.classList.remove("popup_is-opened");
+  buttonMobileMenuMain.classList.toggle("header__menu-mobile_off");
 });
 
 auth.addEventListener("click", () => {
   popupEnter.classList.add("popup_is-opened");
+  mobileMenuMain.classList.toggle("header__buttons_open");
+  headerLogo.classList.toggle("header_dark");
+  buttonCloseMobileMenu.classList.remove("header__menu-mobile_on");
 });
 
 registration.addEventListener("click", () => {
